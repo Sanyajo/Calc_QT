52707de81ae6a3f1f9ba2a2984aaca3628ec9adc
Sanyajo <83663932+Sanyajo@users.noreply.github.com>
2023-01-21 01:53:00 +0300

    new_file

---
+++ stylehelper.h
@@ -0,0 +1,14 @@
+#ifndef STYLEHELPER_H
+#define STYLEHELPER_H
+
+#include <QString>
+
+class StyleHelper
+{
+public:
+    static QString getButtonsStyle();
+    static QString digitalButtons();
+    static QString labelstyle();
+};
+
+#endif // STYLEHELPER_H
